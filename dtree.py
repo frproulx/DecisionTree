1fe76a09f92766380e85b33e7394549a551ac6fc
Frank Proulx <fproulx@tooledesign.com>
2017-07-12 14:22:56 -0700

    Init Commit

---
+++ dtree.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pydot
+
+class dtree(object):
+
+    def __init__(self, data):
+        self.df = data
+        return None
+
+    def _draw(self, parent_name, child_name):
+        edge = pydot.Edge(parent_name, child_name)
+        graph.add_edge(edge)
+
+    def _visit(self, node, parent=None):
+        for k, v in node.iteritems():
+            if isinstance(v, dict):
+                if parent:
+                    self._draw(parent, k)
+                self._visit(v, k)
+        else:
+            self._draw(parent, k)
+            draw(k, k+'_'+v)
+
+    def constructTree(self, splits):
+
+
+    def visualizeTree(self, output_path='./output.png'):
+        graph = pydot.Dot(graph_type='graph')
+        self._visit(output_tree)
+        graph.write_png(output_path)
+        return None
