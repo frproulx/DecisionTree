2ff0f4d5ef3d109b26c3feeafeb878354d79ce66
Frank Proulx <fproulx@gmail.com>
2017-07-19 10:57:48 -0700

    Updated test to reflect new API.

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import dtree
 import numpy as np
 import pandas as pd
-
 import jenkspy
+
+from xlsxwriter import Workbook
 
 crash_data = pd.read_excel('C:/Users/fproulx/Working/MiDOT_Sidepaths/20170706_Kent and Oakland data.xlsx',
                            sheetname='Raw cleaned', skiprows=2)
@@ -16,25 +17,18 @@ output_folder = 'C:/Users/fproulx/Desktop/MiDOT_Sidepaths/trees/'
 
 tree.data.columns
 
-tree_formulae = [('BikeDirection', ['Intersection_or_Non_Intersection_or_Driveway',
+tree_formulae = {'BikeDirection': ['Intersection_or_Non_Intersection_or_Driveway',
                                     'Bike_Facility_Type',
-                                    'Direction_of_Travel']),
-                 ('BikeAction', ['Intersection_or_Non_Intersection_or_Driveway',
+                                    'Direction_of_Travel'],
+                 'BikeAction': ['Intersection_or_Non_Intersection_or_Driveway',
                                                      'Bike_Facility_Type',
-                                                     'Bike_Action']),
-                 ('VehicleAction', ['Intersection_or_Non_Intersection_or_Driveway',
+                                                     'Bike_Action'],
+                 'VehicleAction': ['Intersection_or_Non_Intersection_or_Driveway',
                                                      'Bike_Facility_Type',
-                                                     'Vehicle_action']),
-                 ('CrashType', ['Intersection_or_Non_Intersection_or_Driveway',
+                                                     'Vehicle_action'],
+                 'CrashType': ['Intersection_or_Non_Intersection_or_Driveway',
                                                      'Bike_Facility_Type',
-                                                     'Bike_Crash_Coding'])]
+                                                     'Bike_Crash_Coding']}
 
-tree.split_tree(tree_formulae[0][1])
-tree.to_excel('./test.xlsx', 'test')
 
-for name, columns in tree_formulae:
-    print name
-    tree.split_tree(columns,
-                    reset=True)
-    tree.to_text(output_folder + name + '.txt')
-#    tree.to_png(output_folder + name + '.pdf')
+tree.many_trees_to_excel(tree_formulae, Workbook('./test.xlsx'))
